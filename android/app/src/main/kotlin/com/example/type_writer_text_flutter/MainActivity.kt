8a526d01105dc0dcc9c28dddbaadc3d36e18822d
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-08-31 08:43:46 +0600

    Done commit

---
+++ android/app/src/main/kotlin/com/example/type_writer_text_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.type_writer_text_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:type_writer_text_flutter/typewritertext.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Typewritertext Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TypeWriterText(
+                          repeat: false,
+                          text: Text("Hello and Welcome.",
+                              textAlign: TextAlign.left,
+                              style: Theme.of(context).textTheme.titleMedium),
+                          duration: const Duration(milliseconds: 50)),
+                      TypeWriterText(
+                          repeat: false,
+                          text: Text("My name is Azharul Islam. ",
+                              textAlign: TextAlign.left,
+                              style: Theme.of(context).textTheme.headlineLarge),
+                          duration: const Duration(milliseconds: 100)),
+                      TypeWriterText(
+                          repeat: false,
+                          text: Text("Builder of things. ",
+                              textAlign: TextAlign.left,
+                              style: Theme.of(context).textTheme.headlineLarge),
+                          duration: const Duration(milliseconds: 50)),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TypeWriterText(
+                            repeat: false,
+                            text: Text(
+                                "I'm a full-stack web and salesforce developer, schrum master, and solution architext.",
+                                textAlign: TextAlign.left,
+                                style: Theme.of(context).textTheme.titleMedium),
+                            duration: const Duration(milliseconds: 50)),
+                      ),
+                    ],
+                  )))),
+    );
+  }
+}
